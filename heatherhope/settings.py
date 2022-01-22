3ae2cfd6874d8a43ef3f89fb81824e56e9697886
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-01-22 15:24:16 +0000

    Deployment Commit

--- heatherhope/settings.py
+++ heatherhope/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'heatherhope.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
